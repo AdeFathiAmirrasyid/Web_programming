f9fab32b4b86ba840324933535f081a2b32e31d7
AdeFathiAmirrasyid <adefathi.amirrasyid11@gmail.com>
2022-04-22 21:50:08 +0700

    belajar app contact bagian 2 add data

---
+++ Node_js/18-express-contact-app -2/utils/contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+// Membuat folder data jika belum ada
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+// membuat file contacts.json jika belum ada
+const dataPath = "./data/contacts.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
+
+// mengambil semua data di contact.json
+const loadContact = () => {
+  const file = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
+// mencari contact bedasarkan nama
+const findContact = (nama) => {
+  const contacts = loadContact();
+  const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+  return contact;
+};
+
+// menuliskan / menimpa file contacts.json dengan data yang baru
+const saveContacts = (contacts) => {
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+};
+
+// menambahkan data contact baru
+const addContact = (contact) => {
+  const contacts = loadContact();
+  contacts.push(contact);
+  saveContacts(contacts);
+};
+
+// check nama yang duplicate
+const checkDuplicate = (nama) => {
+  const contacts = loadContact();
+  return contacts.find((contact) => contact.nama === nama);
+};
+
+module.exports = { loadContact, findContact, addContact, checkDuplicate };
