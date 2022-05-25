261167359090035418278ff2f3e05b67364a7d35
AdeFathiAmirrasyid <adefathi.amirrasyid11@gmail.com>
2022-05-25 09:07:31 +0700

    update

--- belajar_laravel/resources/views/dashboard/posts/edit.blade.php
+++ belajar_laravel/resources/views/dashboard/posts/edit.blade.php
@@ -22,7 +22,7 @@
 
             <div class="mb-3">
                 <label for="slug" class="form-label">Slug</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="slug" name="slug" required
+                <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required
                     value="{{ old('slug', $post->slug) }}">
                 @error('slug')
                     <div class="invalid-feedback">{{ $message }} </div>
